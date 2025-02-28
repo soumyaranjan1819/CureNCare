011169fbaf68502c46f347e0ba3697f688f2e0db
sudhansu97ssp <patisudhansu1996@gmail.com>
2025-02-28 09:58:37 +0530

    changed text color

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center text-[#3d3d3d]">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline text-center text-red-500">Hello world!</h1>
     </>
   );
 }
